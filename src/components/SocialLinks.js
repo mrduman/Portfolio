44d4f6464d1ea9d02b4a629a2968cd6f1a5aeb16
mrduman <omer.duman@gmx.de>
2023-04-29 12:23:29 +0200

    Social links

---
+++ src/components/SocialLinks.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+
+function SocialLinks() {
+  const links = [
+    {
+      id: 1,
+      child: (
+        <>
+          Linkedin <FaLinkedin size={30} />
+        </>
+      ),
+      href: "https://www.linkedin.com/",
+      style: "rounded-tr-md",
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          GitHub <FaGithub size={30} />
+        </>
+      ),
+      href: "https://www.github.com/",
+    },
+    {
+      id: 3,
+      child: (
+        <>
+          Mail <HiOutlineMail size={30} />
+        </>
+      ),
+      href: "mail:to:omer.duman@gmx.de",
+    },
+    {
+      id: 4,
+      child: (
+        <>
+          Mail <BsFillPersonLinesFill size={30} />
+        </>
+      ),
+      href: "mail:to:omer.duman@gmx.de",
+      style: "rounded-br-md",
+      download: true,
+    },
+  ];
+
+  return (
+    <div className="flex flex-col top-[35%] lef-0 fixed">
+      <ul>
+        {links.map(({ id, child, style, download, href }) => {
+          return (
+            <li
+              className={
+                "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
+                " " +
+                style
+              }
+            >
+              <a
+                href={href}
+                className="flex justify-between items-center w-full text-white"
+                download={download}
+                target="_blank"
+              >
+                {child}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default SocialLinks;
