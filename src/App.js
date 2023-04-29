6547ede5932961990d1cb8361731c40dbda739cd
mrduman <omer.duman@gmx.de>
2023-04-29 11:55:41 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div>
-      <h1 className="text-4xl font-bold font-signature">Hey</h1>
+      <Navbar />
+      <Home />
     </div>
   );
 }
