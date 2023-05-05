8bab6d07d29493d84d30f3b17474b1dd0ef121fe
mrduman <omer.duman@gmx.de>
2023-05-05 18:14:17 +0200

    wip

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,9 +9,9 @@ function Home() {
       name="home"
       className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
     >
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+      <div className="flex flex-col items-center justify-center h-full max-w-screen-lg mx-auto md:flex-row">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-white leading-tight md:leading-tighter">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white mt-20 md:mt-0">
             I'm a Frontend Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
