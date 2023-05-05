f6253a7ef719256e2d5181fb18478cb549164a6e
mrduman <omer.duman@gmx.de>
2023-05-05 18:01:02 +0200

    some styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,8 +11,8 @@ function Home() {
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I"m a Frontend Developer
+          <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-white leading-tight md:leading-tighter">
+            I'm a Frontend Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
             A dedicated frontend developer with a willingness to learn and
